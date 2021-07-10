cd0496239b6ced502bcffd86707a9539325563a9
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-07-10 14:10:54 +0200

    first commit today

--- start.ps1
+++ start.ps1
@@ -3,13 +3,13 @@ Write-Host "Bienvenue dans le script de lancement des application, profile" -Bac
 $isMode = Read-Host "Tu veut une application [app] ou un profile [profil] ou un ficher [file] ? "
 if($isMode -eq "app") 
 { 
-  .\app\appControlle.ps1
+    .\launch\app\appControlle.ps1
 }elseif ($isMode -eq "profil"){
-  .\profil\profilControlle.ps1
+    .\launch\profil\profilControlle.ps1
 }elseif ($isMode -eq "file"){
-  .\file\fileControlle.ps1
+    .\launch\file\fileControlle.ps1
 }else{
 
-Write-Host "je suis desole mais je ne comprend pas ce que tu veut faire, dans les crochet il y a ce que tu dois mettre"
+Write-Host "je suis desole mais je ne comprend pas ce que tu veut faire, dans les crochet il y a ce que tu dois mettre" -ForegroundColor White -BackgroundColor Red
 
 }
